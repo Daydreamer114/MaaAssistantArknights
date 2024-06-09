2e278058286f255cb624c7af9a4795209385a47a
SherkeyXD <253294679@qq.com>
2024-06-09 21:41:34 +0800

    fix: 修复时间比较报错的问题

--- src/MaaWpfGui/Main/AsstProxy.cs
+++ src/MaaWpfGui/Main/AsstProxy.cs
@@ -772,7 +772,7 @@ namespace MaaWpfGui.Main
                             }
 
                             var interval = recoveryTime - DateTimeOffset.Now.AddMinutes(6);
-                            if (interval > 0) {
+                            if (interval > TimeSpan.Zero) {
                                 _toastNotificationTimer = new DispatcherTimer
                                 {
                                     Interval = interval,
